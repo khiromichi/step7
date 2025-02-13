e13d98675ec7660b973300a05470dff3c4a084a9
hiro <hiro@192.168.10.108>
2025-02-13 11:26:28 +0900

    step7の提出物

--- trainingspring1/src/main/java/com/example/demo/controller/UserController.java
+++ trainingspring1/src/main/java/com/example/demo/controller/UserController.java
@@ -1,14 +1,24 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.dto.UserRequest;
 import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 
 /**
@@ -23,6 +33,9 @@ public class UserController {
   @Autowired
   private UserService userService;
 
+  @Autowired
+  private UserRepository userRepository;
+  
   /**
    * ユーザー情報一覧画面を表示
    * @param model Model
@@ -42,7 +55,31 @@ public class UserController {
    */
   @GetMapping(value = "/user/add")
   public String displayAdd(Model model) {
+    model.addAttribute("userRequest", new UserRequest());
     return "user/add";
+  }
+
+  /**
+   * ユーザー新規登録
+   * @param userRequest リクエストデータ
+   * @param model Model
+   * @return ユーザー情報一覧画面
+   */
+  @RequestMapping(value = "/user/create", method = RequestMethod.POST)
+  public String create(@Validated @ModelAttribute UserRequest userRequest, BindingResult result, Model model) {
+
+    if (result.hasErrors()) {
+      // 入力チェックエラーの場合
+      List<String> errorList = new ArrayList<String>();
+      for (ObjectError error : result.getAllErrors()) {
+        errorList.add(error.getDefaultMessage());
+      }
+      model.addAttribute("validationError", errorList);
+      return "user/add";
+    }
+    // ユーザー情報の登録
+    userService.create(userRequest);
+    return "redirect:/user/list";
   }
 
   /**
@@ -52,9 +89,16 @@ public class UserController {
    * @return ユーザー情報詳細画面
    */
   @GetMapping("/user/{id}")
+//  public String displayView(@PathVariable Long id, Model model) {   
+//    return "user/view";
+//  }                             ここのコードを以下のコードに修正したら詳細の内容が表示されるようになった
   public String displayView(@PathVariable Long id, Model model) {
-    User user = userService.findById(id);
-    model.addAttribute("userData", user);
-    return "user/view";
+	    Optional<User> user = userRepository.findById(id);
+	    if (user.isPresent()) {
+	        model.addAttribute("userData", user.get());  // `userData` を追加
+	        return "user/view";
+	    } else {
+	        return "redirect:/error";
+	    }
   }
 }
